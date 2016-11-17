7348137f334e9c488937afd088c8fddde85539ac
radiolok <radiolok@yandex.ru>
2016-11-17 22:57:24 +0300

    FIX: BFRUN perform helloworld correctly

--- bfrun/bfrun.cpp
+++ bfrun/bfrun.cpp
@@ -7,6 +7,7 @@
 //============================================================================
 
 #include <iostream>
+#include <iomanip>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -24,11 +25,43 @@
 
 using namespace std;
 
+bool ProtectedModeEnabled = false;
+
+void SetProtectedMode(bool mode){
+     ProtectedModeEnabled = mode;
+}
+
+bool GetProtectedMode(void){
+     return ProtectedModeEnabled;
+}
+//Set 16-bit word mode:
+bool WordModeEnabled = false;
+
+void SetWordMode(bool mode){
+     WordModeEnabled = mode;
+}
+
+bool GetWordMode(void){
+     return WordModeEnabled;
+}
+
+void swapLEtoBE(WordToBigEndian_t *array, size_t size){
+
+	uint8_t  tmp = 0;
+	for (size_t i = 0; i < size/sizeof(uint16_t); ++i){
+		tmp = array[i].Byte.low;
+		array[i].Byte.low = array[i].Byte.high;
+		array[i].Byte.high = tmp;
+	}
+
+}
+
 uint8_t  LoadImage(std::ifstream &File, BfHeader_t *Header, uint16_t *MemoryPtr){
 	uint8_t status = 0;
 
 	File.read(reinterpret_cast<char *>(Header), sizeof(BfHeader_t));
 
+	swapLEtoBE((WordToBigEndian_t*)(Header), sizeof(BfHeader_t));
 	BfSection_t Code;
 	Code.Position.Byte.high = Header->Code.Position.Byte.high;
 	Code.Position.Byte.low = Header->Code.Position.Byte.low;
@@ -44,8 +77,10 @@ uint8_t  LoadImage(std::ifstream &File, BfHeader_t *Header, uint16_t *MemoryPtr)
 	Data.Length.Byte.low = Header->Data.Length.Byte.low;
 
 	File.read(reinterpret_cast<char *>(MemoryPtr+Code.Position.Word), sizeof(uint16_t)*Code.Length.Word);
+	swapLEtoBE((WordToBigEndian_t*)(MemoryPtr+Code.Position.Word),sizeof(uint16_t)*Code.Length.Word);
 	if (Data.Length.Word != 0){
 		File.read(reinterpret_cast<char *>(MemoryPtr+Data.Position.Word), sizeof(uint16_t)*Data.Length.Word);
+		swapLEtoBE((WordToBigEndian_t*)(MemoryPtr+Data.Position.Word), sizeof(uint16_t)*Data.Length.Word);
 	}
 
 	return status;
@@ -66,7 +101,7 @@ bool SegFault(const BfSection_t &section, uint16_t Ptr){
 	if ((Ptr < section.Position.Word) ||(Ptr > section.Position.Word + section.Length.Word)){
 		result = true;
 	}
-	return result;
+	return (GetProtectedMode()? result : false);
 }
 
 uint8_t ExecCmd(BfHeader_t *Header, uint16_t *MemoryPtr, uint16_t &IP, uint16_t &AP){
@@ -75,45 +110,64 @@ uint8_t ExecCmd(BfHeader_t *Header, uint16_t *MemoryPtr, uint16_t &IP, uint16_t 
 	uint16_t cmd_bin = ((cmd>>13) & 0x0007);
 	bool sign = ((cmd >> 12)&0x01)? true: false;
 	uint16_t bias = (cmd&0x0FFF);
-
 	switch (cmd_bin){
-	case 0:
+	case 0:// '<' and '>' commands
 		MemoryPtr[AP] = sign? MemoryPtr[AP] - bias: MemoryPtr[AP] + bias;
 		break;
-	case 1:
+	case 1:// '+' and '-' commands
 		AP = sign? AP - bias: AP + bias;
 		if (SegFault(Header->Data, AP)){
 			return -1;
 		}
 		break;
-	case 2:
-		MemoryPtr[AP] = In();
-		break;
-	case 3:
-		Out(MemoryPtr[AP]);
-		break;
-	case 4:
-		IP = MemoryPtr[AP]? IP : (sign? IP - bias: IP + bias);
-		if (SegFault(Header->Data, IP)){
-			return -1;
+	case 2://Console input cmd
+		if (GetWordMode()){
+			MemoryPtr[AP] = In();		
+		}
+		else{
+			MemoryPtr[AP] = In() & 0xFF;
 		}
 		break;
-	case 5:
-		IP = MemoryPtr[AP]? (sign? IP - bias: IP + bias) : IP;
-		if (SegFault(Header->Data, IP)){
-			return -1;
+	case 3://console output cmd
+		if (GetWordMode()){
+			Out(MemoryPtr[AP]);
+		}
+		else{
+			Out(MemoryPtr[AP] & 0xFF);
 		}
 		break;
-	case 6:
+	case 4://Jump If Zero
+		if (GetWordMode()){
+			IP = MemoryPtr[AP]? IP : (sign? IP - bias: IP + bias);
+		}
+		else{
+			IP = (MemoryPtr[AP] & 0xFF)? IP : (sign? IP - bias: IP + bias);
+		}
+		if (SegFault(Header->Data, IP)){
+			return -2;
+		}
+		break;
+	case 5://Jump If not zero
+		if (GetWordMode()){
+			IP = MemoryPtr[AP]? (sign? IP - bias: IP + bias) : IP;
+		}
+		else{
+			IP = (MemoryPtr[AP] & 0xFF)? (sign? IP - bias: IP + bias) : IP;
+		}
+		if (SegFault(Header->Data, IP)){
+			return -3;
+		}
+		break;
+	case 6://Set IP
 		IP = bias;
 		if (SegFault(Header->Data, IP)){
-			return -1;
+			return -4;
 		}
 		break;
-	case 7:
+	case 7://Set AP
 		AP = Header->Data.Position.Word + bias;
 		if (SegFault(Header->Data, AP)){
-			return -1;
+			return -5;
 		}
 		break;
 	}
@@ -125,9 +179,9 @@ uint8_t ExecCode(BfHeader_t *Header, uint16_t *MemoryPtr){
 
 	uint16_t IP = Header->Code.Position.Word;
 	uint16_t AP = Header->Data.Position.Word;
-
+	size_t i = 0;
 	do {
-
+		i++;
 		status = ExecCmd(Header,MemoryPtr, IP, AP);
 		if (status){
 			return -1;
@@ -135,7 +189,7 @@ uint8_t ExecCode(BfHeader_t *Header, uint16_t *MemoryPtr){
 
 		IP++;
 	}while (IP < Header->Code.Position.Word + Header->Code.Length.Word);
-
+	cerr << "\r\nIstructions_retired:" << i << "\r\n";
 	return status;
 }
 
@@ -144,12 +198,18 @@ int main(int argc, char *argv[]) {
 	int status = -1;
 	int c = 0;
 	char filePath[4096] = {0x00};
-	while((c = getopt(argc, argv, "f:c:di")) != -1){
+	while((c = getopt(argc, argv, "f:px")) != -1){
 		switch(c)
 		{
 		case 'f':
 			strcpy(filePath,optarg);
 			break;
+		case 'p':
+			SetProtectedMode(true);
+		break;
+		case 'x':
+			SetWordMode(true);
+		break;
 		}
 	}
 	if (*filePath == 0){
@@ -161,9 +221,14 @@ int main(int argc, char *argv[]) {
 	memset(&Memory,0,sizeof(Memory));
 	status = LoadBF(filePath,Header,Memory);
 	if (status){
+		cerr << "Load BF buffer Error, Status =" << status << endl;
 		return -1;
 	}
-	ExecCode(Header, Memory);
+	status = ExecCode(Header, Memory);
+	if (status){
+		cerr << "Code Execution Error, Status =" << status << endl;
+		return -1;
+	}
 
 	return 0;
 }
