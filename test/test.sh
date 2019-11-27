f9eac6e20274da343b206401105ca6c0feced8ed
Artem Kashkanov <radiolok@yandex.ru>
2019-11-27 18:10:00 +0300

    Add DekatronPC test

--- test/test.sh
+++ test/test.sh
@@ -4,3 +4,5 @@ path=$(dirname $(dirname $(realpath $0)))
 #compile helloworld:
 $path/bin/bfpp -i $path/common/helloworld.bfk -o $path/helloworld.out || exit 1
 $path/bin/bfrun -f $path/helloworld.out | grep -q 'Hello World!' && exit 0 || exit 1
+$path/bin/dpcrun -f $path/helloworld.bfk | grep -q 'Hello World!' && exit 0 || exit 1
+
