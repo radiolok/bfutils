a85e8d4d25635d5d3c5f2ae71679377d1cd1a372
Artem Kashkanov <radiolok@yandex.ru>
2019-11-27 11:27:41 +0300

    Add integration test

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
+
 path=$(dirname $(dirname $(realpath $0)))
-echo $path
 #compile helloworld:
 $path/bin/bfpp -i $path/common/helloworld.bfk -o $path/helloworld.out || exit 1
-$path/bin/bfrun -i $path/helloworld.out > $path/result || exit 1
+$path/bin/bfrun -f $path/helloworld.out | grep -q 'Hello World!' && exit 0 || exit 1
