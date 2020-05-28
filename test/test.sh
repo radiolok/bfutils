51ad46a55d66cf422c78edbb9bd1675d16ae2f9d
Artem Kashkanov <radiolok@yandex.ru>
2020-05-28 18:36:47 +0300

    Add DekatronPC C++ emulation test

--- test/test.sh
+++ test/test.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-path=$(dirname $(dirname $(realpath $0)))
+path="$(dirname "$(dirname "$(realpath $0)")")"
+
+
+function nope {
+  return
+} 
+
 #compile helloworld:
-$path/bin/bfpp -i $path/common/helloworld.bfk -o $path/helloworld.out || exit 1
-$path/bin/bfrun -f $path/helloworld.out | grep -q 'Hello World!' && exit 0 || exit 1
-$path/bin/dpcrun -f $path/helloworld.bfk | grep -q 'Hello World!' && exit 0 || exit 1
+$path/bin/bfpp -i "$path/common/helloworld.bfk" -o "$path/helloworld.out" || exit 1
+$path/bin/bfrun -f "$path/helloworld.out" | grep -q 'Hello World!' || exit 1
+
+$path/bin/bfpp -i "$path/common/pi.bfk" -o "$path/pi.out" || exit 1
+$path/bin/bfrun -f "$path/pi.out" | grep -q '3.141' || exit 1
+
+#check dpcrun:
+$path/bin/dpcrun -f "$path/common/helloworld.bfk" | grep -q 'Hello World!' || exit 1
+
+$path/bin/dpcrun -f "$path/common/pi.bfk" | grep -q '3.141' || exit 1
+exit 0
 
